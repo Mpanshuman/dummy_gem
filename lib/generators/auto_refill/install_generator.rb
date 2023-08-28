847b26b4d06e475e707e690704d61d423092f066
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-08-28 16:04:53 +0530

    update

--- lib/generators/auto_refill/install_generator.rb
+++ lib/generators/auto_refill/install_generator.rb
@@ -7,34 +7,15 @@ module AutoRefill
   module Generators
 
     class InstallGenerator < Rails::Generators::Base
-      argument :attributes, type: :array, default: [], banner: "field:type field:type"
-
-      class_option :primary_key_type, type: :string, desc: "The type for primary key"
-
-      source_root File.expand_path("../templates", __FILE__)
-
-      def copy_auto_refill_migration
-        if (behavior == :invoke && model_exists?) || (behavior == :revoke && migration_exists?(table_name))
-          migration_template "migration_existing.rb", "#{migration_path}/add_devise_to_#{table_name}.rb", migration_version: migration_version
-        else
-          migration_template "migration.rb", "#{migration_path}/devise_create_#{table_name}.rb", migration_version: migration_version
-        end
+      source_root File.expand_path('../templates', __dir__)
+      def create_migration
+        migration_template 'migration_template.rb', "db/migrate/#{migration_file_name}.rb"
       end
-
-      def generate_model
-        invoke "active_record:model", [name], migration: false unless model_exists? && behavior == :invoke
+      def migration_file_name
+        'add_custom_field_to_#{table_name}'
       end
-
-      def migration_data
-        <<RUBY
-          ## Database authenticatable
-          t.bigint :order_id,              null: false, default: nil
-          t.date :start_date,              null: false, default: nil
-          t.string :status,              null: false, default: 'active'
-          ## Rememberable
-          t.datetime :remember_created_at
-
-        RUBY
+      def table_name
+        'auto_refill'
       end
     end
   end
--- lib/generators/templates/migration.rb
+++ lib/generators/templates/migration.rb
@@ -1,5 +1,11 @@
 class AutoRefillCreate<%= table_name.camelize %> < ActiveRecord::Migration[<%= Rails::VERSION::MAJOR %>.<%= Rails::VERSION::MINOR %>]
   def change
     # Your migration code here
+    create_table :<%= table_name %> do |t|
+
+    t.bigint :order_id
+    t.date :start_date
+    t.string :status
+
   end
 end
