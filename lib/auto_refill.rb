174912422f218ecb3657e81a3ea46777f2be64fd
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-08-28 11:17:53 +0530

    added git configs

---
+++ lib/auto_refill.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "auto_refill/version"
+
+module AutoRefill
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/auto_refill/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AutoRefill
+  VERSION = "0.1.0"
+end
