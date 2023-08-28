afdf62a9617957c8537913aac7f70eb5d74ddfd9
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-08-28 15:48:50 +0530

    update

--- lib/generators/auto_refill/install_generator.rb
+++ lib/generators/auto_refill/install_generator.rb
@@ -7,8 +7,34 @@ module AutoRefill
   module Generators
 
     class InstallGenerator < Rails::Generators::Base
-      def code_that_runs
-        puts "Hi"
+      argument :attributes, type: :array, default: [], banner: "field:type field:type"
+
+      class_option :primary_key_type, type: :string, desc: "The type for primary key"
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_auto_refill_migration
+        if (behavior == :invoke && model_exists?) || (behavior == :revoke && migration_exists?(table_name))
+          migration_template "migration_existing.rb", "#{migration_path}/add_devise_to_#{table_name}.rb", migration_version: migration_version
+        else
+          migration_template "migration.rb", "#{migration_path}/devise_create_#{table_name}.rb", migration_version: migration_version
+        end
+      end
+
+      def generate_model
+        invoke "active_record:model", [name], migration: false unless model_exists? && behavior == :invoke
+      end
+
+      def migration_data
+        <<RUBY
+          ## Database authenticatable
+          t.bigint :order_id,              null: false, default: nil
+          t.date :start_date,              null: false, default: nil
+          t.string :status,              null: false, default: 'active'
+          ## Rememberable
+          t.datetime :remember_created_at
+
+        RUBY
       end
     end
   end
--- lib/generators/auto_refill_generator.rb
+++ lib/generators/auto_refill_generator.rb
@@ -23,71 +23,17 @@ module ActiveRecord
         invoke "active_record:model", [name], migration: false unless model_exists? && behavior == :invoke
       end
 
-      def inject_devise_content
-        content = model_contents
-
-        class_path = if namespaced?
-          class_name.to_s.split("::")
-        else
-          [class_name]
-        end
-
-        indent_depth = class_path.size - 1
-        content = content.split("\n").map { |line| "  " * indent_depth + line } .join("\n") << "\n"
-
-        inject_into_class(model_path, class_path.last, content) if model_exists?
-      end
-
       def migration_data
-<<RUBY
-      ## Database authenticatable
-      t.bigint :order_id,              null: false, default: nil
-      t.date :start_date,              null: false, default: nil
-      t.string :status,              null: false, default: 'active'
-      ## Rememberable
-      t.datetime :remember_created_at
+      <<RUBY
+        ## Database authenticatable
+        t.bigint :order_id,              null: false, default: nil
+        t.date :start_date,              null: false, default: nil
+        t.string :status,              null: false, default: 'active'
+        ## Rememberable
+        t.datetime :remember_created_at
 
-RUBY
+      RUBY
       end
-
-      def rails5_and_up?
-        Rails::VERSION::MAJOR >= 5
-      end
-
-      def rails61_and_up?
-        Rails::VERSION::MAJOR > 6 || (Rails::VERSION::MAJOR == 6 && Rails::VERSION::MINOR >= 1)
-      end
-
-      def postgresql?
-        ar_config && ar_config['adapter'] == 'postgresql'
-      end
-
-      def ar_config
-        if ActiveRecord::Base.configurations.respond_to?(:configs_for)
-          if rails61_and_up?
-            ActiveRecord::Base.configurations.configs_for(env_name: Rails.env, name: "primary").configuration_hash
-          else
-            ActiveRecord::Base.configurations.configs_for(env_name: Rails.env, spec_name: "primary").config
-          end
-        else
-          ActiveRecord::Base.configurations[Rails.env]
-        end
-      end
-
-     def migration_version
-       if rails5_and_up?
-         "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
-       end
-     end
-
-     def primary_key_type
-       primary_key_string if rails5_and_up?
-     end
-
-     def primary_key_string
-       key_string = options[:primary_key_type]
-       ", id: :#{key_string}" if key_string
-     end
     end
   end
 end
