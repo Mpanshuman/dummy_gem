4d64d72a81c4c31b1e371a3faa02a90008bc583e
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-08-31 15:30:31 +0530

    update

--- lib/generators/auto_refill/install_generator.rb
+++ lib/generators/auto_refill/install_generator.rb
@@ -18,7 +18,8 @@ module AutoRefill
       def table_name
         'auto_refill'
       end
-      def next_migration_number(dirname)
+      private
+      def self.next_migration_number(dirname)
         next_migration_number = current_migration_number(dirname) + 1
         ActiveRecord::Migration.next_migration_number(next_migration_number)
       end
