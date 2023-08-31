c220c33dba30ece4d8db26890b41461aa153e84f
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-08-31 15:54:32 +0530

    update

--- lib/generators/templates/migration.rb
+++ lib/generators/templates/migration.rb
@@ -1,11 +1,9 @@
-class AutoRefillCreate<%= table_name.camelize %> < ActiveRecord::Migration[<%= Rails::VERSION::MAJOR %>.<%= Rails::VERSION::MINOR %>]
+class CreateAutoRefill < ActiveRecord::Migration[<%= Rails::VERSION::MAJOR %>.<%= Rails::VERSION::MINOR %>]
   def change
-    # Your migration code here
     create_table :<%= table_name %> do |t|
-
     t.bigint :order_id
     t.date :start_date
     t.string :status
-
+    end
   end
 end
