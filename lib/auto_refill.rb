97ca337a8ac53cf7cd03cabb01d4c0e33cbe7b47
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-09-01 10:31:02 +0530

    update

--- lib/auto_refill.rb
+++ lib/auto_refill.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
-require 'hola/create'
+require 'auto_refill/refill'
 require_relative "auto_refill/version"
 
 module AutoRefill
-  def check_gem(value)
-    refill = Refill.new(3,'some date')
+  def check_gem(source_id, start_date)
+    refill = Refill.new(source_id, start_date)
     refill.create_auto_refill
   end
 end
