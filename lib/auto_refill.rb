a5cba4bb3869145c3b8f5555d1809ca0deea9e55
MP Anshuman (NTE-393) <mp.anshuman@nettantra.net>
2023-09-11 15:56:29 +0530

    update

--- lib/auto_refill.rb
+++ lib/auto_refill.rb
@@ -7,9 +7,13 @@ module AutoRefill
   def check_gem(source_id, start_date)
     # refill = Refill.new(source_id, start_date)
     # refill.create_auto_refill
-    order_all = Order.all
+    order_all = Order.all.last
+    refill = auto_refill.create!({
+      order_id: order_all.id,
+      status: 'active',
+    })
     print "----------------------------------- \n"
-    print "all order : #{order_all} \n"
+    print "all order : #{refill} \n"
     print "----------------------------------- \n"
   end
 end
